0904bcf42607fd64eb9eab9ed5c4ea0ce3457661
bubucuo <276185876@qq.com>
2020-02-23 10:16:28 +0800

    delete lesson2-save

--- 16期/lesson2-save/src/themeContext.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const ThemeContext = React.createContext({themeColor: "pink"});
-export const ThemeProvider = ThemeContext.Provider;
-export const ThemeConsumer = ThemeContext.Consumer;
